8d2e89e09a558661c49cf8b03c53b33b63a9a4d7
Santiago Bernabé <santiago.bernabe@medianet.es>
2016-09-15 22:20:31 +0200

    First full game version

--- app/services/photos.js
+++ app/services/photos.js
@@ -4,16 +4,39 @@ angular.module('myApp')
 
 .factory('Photos', ['$resource',
     function($resource) {
-      return $resource('https://api.500px.com/v1/photos', {}, {
-        query: {
-          method: 'GET',
-          params: {
-            feature: 'popular',
-            consumer_key: 'QJkue2FcLLCIThSa55zjqnqIiVpncLgaS6rfkwOt',
-            tags: 1,
-            image_size: 440
-          }
+
+      var PARAMS = {
+            query: {
+              method: 'GET',
+              params: {
+                feature: 'popular',
+                consumer_key: 'QJkue2FcLLCIThSa55zjqnqIiVpncLgaS6rfkwOt',
+                tags: 1,
+                image_size: 440
+              }
+            }
+          };
+      var photos;
+
+      return {
+        fetch: function () {
+          
+          var promise = $resource('https://api.500px.com/v1/photos', {}, PARAMS)
+                            .query()
+                            .$promise;
+
+          promise.then(function (data) {
+            photos = data.photos;
+          });
+
+          return promise;
+        },
+
+        get: function () {
+          return photos || [];
         }
-      });
+      }
+
+      
     }
   ])
--- app/views/game/game.js
+++ app/views/game/game.js
@@ -9,367 +9,211 @@ angular.module('myApp.game', ['ngRoute'])
   });
 }])
 
-.controller('GameCtrl', ['Photos', '$location', '$rootScope', 'GameResults', function(Photos, $location, $rootScope, GameResults) {
+.controller('GameCtrl', ['Photos', '$location', '$rootScope', 'GameResults', 'Game.Preloader', 'Game.Logic', function(Photos, $location, $rootScope, GameResults, Preloader, Logic) {
 
-  var photos;
-  var $ = angular.element;
-
-  Photos.query()
-    .$promise.then(function(data) {
-
-      photos = data.photos.map(function (obj) {
+  Photos.fetch()
+    .then(function(data) {
+      return data.photos.map(function (obj) {
         return obj.image_url;
       });
-
-      console.log(photos)
-
-      loadGame();
     })
+    .then(function (photosURL) {
 
+      var game = new Phaser.Game('100%', '100%', Phaser.CANVAS, 'game', null,true);
 
-  var Clicker = {
-      score: 0
-  };
+      game.state.add('Preloader', Preloader(photosURL), true);
+      game.state.add('Game', Logic);
 
-  var game;
+    });
+}])
 
-  Clicker.Preloader = function () {};
+.factory('Game.Preloader', function() {
 
-  Clicker.Preloader.prototype = {
+  var _photosURL;
+
+  var Preloader = function () {};
+
+  Preloader.prototype = {
 
       init: function () {
-
           this.input.maxPointers = 1;
           this.scale.pageAlignHorizontally = true;
-
       },
 
       preload: function () {
 
-          this.load.path = 'app/assets/';
+        this.load.path = 'app/assets/';
 
-          this.load.image('logo');
+        this.load.bitmapFont('fat-and-tiny');
+        this.load.bitmapFont('digits');
 
-          this.load.bitmapFont('fat-and-tiny');
-          this.load.bitmapFont('digits');
-
-          this.load.shader('ripples');
-
-          this.load.spritesheet('sprites', 'sprites.png', 32, 32);
-
-          this.load.images(photos);
-
+        this.load.images(_photosURL);
       },
 
       create: function () {
+        this.state.start('Game');
+      } 
+  }
 
-          this.state.start('Clicker.MainMenu');
+  return function (photosURL) {
 
-      }
+    _photosURL = photosURL;
 
+    return Preloader;
+  }
+})
+
+.factory('Game.Logic', ['Photos', '$rootScope', '$location', 'GameResults', function(Photos, $rootScope, $location, GameResults) {
+
+  var Game = function () {
+
+    this.score = 0;
+    this.scoreText = null;
+
+    this.bigText = null;
+
+    this.stars = null;
+    this.timer = null;
   };
 
-  Clicker.MainMenu = function () {
+  Game.prototype = {
 
-      this.filter = null;
+    init: function () {
 
-  };
+        this.score = 0;
+    },
 
-  Clicker.MainMenu.prototype = {
+    create: function () {
 
-      create: function () {
+        this.timeText = this.add.bitmapText(this.world.centerX + 32, 128, 'digits', '10', 1024);
+        this.timeText.anchor.set(0.5);
+        this.timeText.smoothed = false;
+        this.timeText.alpha = 0.3;
 
-          this.stage.backgroundColor = 0x000000;
+        this.stars = this.add.group();
 
-          var uniforms = {
-              color: { type: '3fv', value: [ 0.1, 0.3, 0.6 ] }
-          };
+        this.scoreText = this.add.bitmapText(64, 12, 'fat-and-tiny', 'SCORE: 0', 32);
+        this.scoreText.smoothed = false;
+        this.scoreText.tint = 0x000000;
+        //this.scoreText.visible = false;
 
-          this.filter = new Phaser.Filter(this.game, uniforms, this.cache.getShader('ripples'));
-          this.filter.addToWorld(0, 0, 800, 600);
+        this.timer = this.time.create(false);
+        this.timer.add(10000, this.timeUp, this);
+        this.timer.start();
 
-          var logo = this.add.image(this.world.centerX, 48, 'logo');
-          logo.anchor.x = 0.5;
+        this.rawPhotoData = Photos.get();
 
-          var start = this.add.bitmapText(this.world.centerX, 460, 'fat-and-tiny', 'CLICK TO PLAY', 64);
-          start.anchor.x = 0.5;
-          start.smoothed = false;
-          start.tint = 0xff0000;
+        this.playing = true;
+        this.speed = 1;
 
-          var score = this.add.bitmapText(this.world.centerX, 500, 'fat-and-tiny', 'HIGH SCORE: ' + Clicker.score, 64);
-          score.anchor.x = 0.5;
-          score.smoothed = false;
-          score.tint = 0xff0000;
-
-          console.log(game.cache.getKeys(Phaser.Cache.IMAGE)[1])
-
-          var photo = this.add.image(this.world.centerX, 48, photos[Math.floor(Math.random() * photos.length)]);
-          logo.anchor.x = 0.5;
-
-          this.input.onDown.addOnce(this.start, this);
-
-      },
-
-      start: function () {
-
-          this.state.start('Clicker.Game');
-
-      },
+        this.currentIndex = 0;
+        this.showNextPhoto();
+    },
 
       update: function () {
 
-          this.filter.update();
-
-      }
-
-  };
-
-  Clicker.Game = function () {
-
-      this.score = 0;
-      this.scoreText = null;
-
-      this.timeText = null;
-
-      this.target = null;
-      this.stars = null;
-      this.timer = null;
-
-      this.tremorRect = null;
-
-      this.colors = { r: 0.1, g: 0.1, b: 0.1 };
-      this.uniform = null;
-      this.filter = null;
-
-      this.pauseKey = null;
-      this.debugKey = null;
-      this.showDebug = false;
-
-  };
-
-  Clicker.Game.prototype = {
-
-      init: function () {
-
-          this.score = 0;
-          this.colors = { r: 0.1, g: 0.3, b: 0.6 };
-      },
-
-      create: function () {
-
-          this.stage.backgroundColor = 0x000000;
-
-          var uniforms = {
-              color: { type: '3fv', value: [ 0.0, 0.0, 0.0 ] }
-          };
-
-          this.filter = new Phaser.Filter(this.game, uniforms, this.cache.getShader('ripples'));
-          this.filter.addToWorld(0, 0, 800, 600);
-
-          this.uniform = this.filter.uniforms.color.value;
-
-          this.timeText = this.add.bitmapText(this.world.centerX + 32, 128, 'digits', '10', 1024);
-          this.timeText.anchor.set(0.5);
-          this.timeText.smoothed = false;
-          this.timeText.alpha = 0.3;
-
-          this.stars = this.add.group();
-
-          this.target = this.add.sprite(400, 200, 'sprites');
-          this.target.inputEnabled = true;
-          this.target.scale.set(2);
-          this.target.smoothed = false;
-
-          this.target.events.onInputDown.add(this.clickedIt, this);
-
-          this.tremorRect = new Phaser.Rectangle(this.target.x, this.target.y, 6, 6);
-
-          this.scoreText = this.add.bitmapText(64, 12, 'fat-and-tiny', 'SCORE: 0', 32);
-          this.scoreText.smoothed = false;
-          this.scoreText.tint = 0xffff00;
-          this.scoreText.visible = false;
-
-          this.timer = this.time.create(false);
-          this.timer.add(10000, this.timeUp, this);
-          this.timer.start();
-
-          //  Press P to pause and resume the game
-          this.pauseKey = this.input.keyboard.addKey(Phaser.Keyboard.P);
-          this.pauseKey.onDown.add(this.togglePause, this);
-
-          //  Press D to toggle the debug display
-          this.debugKey = this.input.keyboard.addKey(Phaser.Keyboard.D);
-          this.debugKey.onDown.add(this.toggleDebug, this);
-
-          //  Start the colors tweening
-          this.add.tween(this.colors).to( { r: 0.2, g: 1.0, b: 0.2 }, 10000, "Linear", true);
-
-      },
-
-      togglePause: function () {
-
-          this.game.paused = (this.game.paused) ? false : true;
-
-      },
-
-      toggleDebug: function () {
-
-          this.showDebug = (this.showDebug) ? false : true;
-
-      },
-
-      clickedIt: function (sprite, pointer) {
-
-          var flash = this.stars.create(pointer.x, pointer.y, 'sprites', 1);
-          flash.anchor.set(0.5);
-
-          this.add.tween(flash).to({ alpha: 0.2 }, 1000, "Linear", true);
-
-          var x = this.rnd.between(0, this.game.width - this.target.width);
-          var y = this.rnd.between(0, this.game.height - this.target.height);
-
-          this.target.x = x;
-          this.target.y = y;
-
-          this.tremorRect.x = x;
-          this.tremorRect.y = y;
-
-      },
-
-      update: function () {
-
-          this.tremorRect.random(this.target);
-
-          if (this.timeText.visible)
-          {
+          if (this.timeText.visible) {
               this.timeText.text = 10 - Math.floor(this.timer.seconds);
           }
 
-          this.uniform[0] = this.colors.r;
-          this.uniform[1] = this.colors.g;
-          this.uniform[2] = this.colors.b;
+          if (this.playing && this.input.keyboard.pressEvent) {
 
-          this.filter.update();
+            var ev = this.input.keyboard.pressEvent;
+            var succes = this.currentTags.some(function (value) {
+              return value.indexOf(ev.key) === 0;
+            })
 
-      },
+            this.keysPressed.push(ev.key);
+            ev = null;
 
-      timeUp: function () {
-
-          this.timeText.text = "";
-          this.timeText.visible = false;
-
-          this.target.inputEnabled = false;
-
-          var tween = this.add.tween(this.target).to({ alpha: 0 }, 1000, "Linear", true);
-
-          this.scoreText.visible = true;
-
-          //  Did they score anything?
-          if (this.stars.total > 0)
-          {
-              tween.onComplete.addOnce(this.displayStars, this);
-          }
-          else
-          {
-              this.time.events.add(2000, this.gameOver, this);
+            if (succes) {
+              this.success();
+            }
           }
 
-      },
-
-      displayStars: function () {
-
-          this._i = 0;
-          this.stars.forEach(this.showStar, this);
-
-      },
-
-      showStar: function (star) {
-
-          this.add.tween(star).to({ alpha: 1 }, 250, "Linear", true, this._i * 250);
-          this.add.tween(star).to({ x: 32  }, 750, "Sine.easeIn", true, (this._i * 250) + 250);
-          var tween = this.add.tween(star).to({ y: 32 }, 750, "Sine.easeOut", true, (this._i * 250) + 250);
-
-          this._i++;
-
-          if (this._i === this.stars.total)
-          {
-              tween.onComplete.add(this.addScore, this, 0, true);
-          }
-          else
-          {
-              tween.onComplete.add(this.addScore, this, 0, false);
+          if (this.playing && this.currentPhoto.x - (this.currentPhoto.width / 2) > this.world.width) {
+            this.gameOver();
           }
 
+          this.currentPhoto.x += this.speed;
       },
 
-      addScore: function (tween, star, lastStar) {
+      success: function () {
 
-          this.score++;
+        this.playing = false;
+
+        var tween = this.add.tween(this.currentPhoto).to({
+          x: this.world.centerX,
+          y: this.world.centerY,
+          width: 10000,
+          height: 10000,
+          alpha: 0
+        }, 300, "Sine.easeOut", true, 100);
+
+        tween.onComplete.add(this.addScore, this);
+      },
+
+      calcPoints: function () {
+
+        var worldWidth = this.world.width;
+        var photoRightCorner = this.currentPhoto.x;
+        var percentageOfScreen = (worldWidth - photoRightCorner) / worldWidth;
+        var positionScore = Math.floor(percentageOfScreen * 10);
+
+        var speedScore = Math.floor(this.speed);
+
+        var keysNeeded = this.keysPressed.length;
+        var keysNeededScore = Math.max(10 - keysNeeded, 0);
+
+        this.totalKeys = this.totalKeys || [];
+        this.totalKeys.concat(this.keysPressed);
+
+        console.log(positionScore, speedScore, keysNeededScore)
+
+        return positionScore + speedScore + keysNeededScore;
+      },
+
+      addScore: function (tween) {
+
+          console.log('termina el twwen')
+          //this.currentPhoto.destroy();
+
+          this.score += this.calcPoints();
           this.scoreText.text = "SCORE: " + this.score;
 
-          if (lastStar)
-          {
-              this.time.events.add(2000, this.gameOver, this);
-          }
+          this.showNextPhoto();
+      },
 
+      showNextPhoto: function () {
+        this.currentIndex += 1;
+        this.currentPhoto = this.add.image(0, this.world.centerY, this.rawPhotoData[this.currentIndex].image_url);
+        this.currentPhoto.anchor.set(0.5);
+        this.currentPhoto.x -= this.currentPhoto.width / 2;
+        this.currentTags = this.rawPhotoData[this.currentIndex].tags;
+
+        this.speed *= 2;
+
+        this.playing = true;
+        this.keysPressed = [];
+
+        console.log(this.currentTags);
       },
 
       gameOver: function () {
 
-          if (this.score > Clicker.score)
-          {
-              Clicker.score = this.score;
-          }
+        var newScore = this.score;
 
-          var score = this.score;
+        GameResults.set({
+          score: newScore
+        });
 
-          GameResults.set({
-            score: score            
-          });
+        this.game.destroy();
 
-          $rootScope.$apply(function() {
-            $location.path('/results');
-          });
-      },
-
-      render: function () {
-
-          if (this.showDebug)
-          {
-              this.game.debug.text("Duration: " + this.timer.seconds, 32, 32);
-              this.game.debug.text("R: " + this.colors.r, 32, 128);
-              this.game.debug.text("G: " + this.colors.g, 32, 128 + 32);
-              this.game.debug.text("B: " + this.colors.b, 32, 128 + 64);
-          }
-
+        $rootScope.$apply(function() {
+          $location.path('/results');
+        });
       }
   };
 
-  $(window).on('resize', resizeGame);
-
-  function resizeGame() {
-    var height = $(window)[0].innerHeight;
-    var width = $(window)[0].innerWidth;
-
-    game.width = width;
-    game.height = height;
-    game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-
-    game.stage._bounds.width = width;
-    game.stage._bounds.height = height;
-  }
-
-
-  function loadGame() {
-
-    game = new Phaser.Game('100%', '100%', Phaser.CANVAS, 'game');
-
-    game.state.add('Clicker.Preloader', Clicker.Preloader);
-    game.state.add('Clicker.MainMenu', Clicker.MainMenu);
-    game.state.add('Clicker.Game', Clicker.Game);
-
-    game.state.start('Clicker.Preloader'); 
-  }
-
-
-}]);
+  return Game;
+}])
